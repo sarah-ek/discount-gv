f720e6b6b0a9e636d7ded3857d95abba8b227a16
kitegi <40109184+kitegi@users.noreply.github.com>
2018-06-09 19:47:17 +0200

    Exclude self posts

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ with open("identifiers.csv") as id_csv:
 
 reddit = bot_login(imported_id)
 subreddit = reddit.subreddit("badmathematics")
-test = reddit.subreddit("testingground4bots")
 
-for submission in test.stream.submissions(skip_existing=True):
-    if submission:
-        archive_url = archiveis.capture(submission.url)
+for submission in subreddit.stream.submissions(skip_existing=True):
+    if submission and len(submission.selftext) == 0:
+        url = submission.url
+        if url.startswith("https://www.reddit.com"):
+            url = url[0:8] + 'old' + url[11:]
+        archive_url = archiveis.capture(url)
         comment_text = f"[Here's]({archive_url}) an archived version of this thread.  \n"\
                        f"[^^Source](https://github.com/kitegi/discount-gv)"
-        submission.reply()
+        submission.reply(comment_text)
         print("Reply sent successfully")
