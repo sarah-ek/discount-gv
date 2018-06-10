874f82223ed13950ceb96004a6d05728e5b6bf2a
kitegi <40109184+kitegi@users.noreply.github.com>
2018-06-10 15:39:47 +0200

    Less frequent requests

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import archiveis
 import praw
 import csv
+from time import sleep
 
 
 def bot_login(identifiers):
@@ -14,14 +15,19 @@ with open("identifiers.csv") as id_csv:
 
 reddit = bot_login(imported_id)
 subreddit = reddit.subreddit("badmathematics")
+submission_stream = subreddit.stream.submissions(skip_existing=True, pause_after=0)
 
-for submission in subreddit.stream.submissions(skip_existing=True):
-    if submission and len(submission.selftext) == 0:
-        url = submission.url
-        if url.startswith("https://www.reddit.com"):
-            url = url[0:8] + 'old' + url[11:]
-        archive_url = archiveis.capture(url)
-        comment_text = f"[Here's]({archive_url}) an archived version of this thread.  \n"\
-                       f"[^^Source](https://github.com/kitegi/discount-gv)"
-        submission.reply(comment_text)
-        print("Reply sent successfully")
+while True:
+    submission = next(submission_stream)
+    if submission:
+        if len(submission.selftext) == 0:
+            url = submission.url
+            if url.startswith("https://www.reddit.com"):
+                url = url[0:8] + 'old' + url[11:]
+            archive_url = archiveis.capture(url)
+            comment_text = f"[Here's]({archive_url}) an archived version of this thread.  \n" \
+                           f"[^^Source](https://github.com/kitegi/discount-gv)"
+            submission.reply(comment_text)
+            print("Reply sent successfully")
+    else:
+        sleep(60)
