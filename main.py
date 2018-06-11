2338695f84f85e41937c80ea354d05ba8643c5f8
kitegi <40109184+kitegi@users.noreply.github.com>
2018-06-11 19:19:18 +0200

    Server error handling

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import praw
 import csv
 from time import sleep
 
+import prawcore
+
 
 def bot_login(identifiers):
     login = praw.Reddit(**identifiers)
@@ -18,16 +20,19 @@ subreddit = reddit.subreddit("badmathematics")
 submission_stream = subreddit.stream.submissions(skip_existing=True, pause_after=0)
 
 while True:
-    submission = next(submission_stream)
-    if submission and not submission.is_self:
-        url = submission.url
-        if url.startswith("https://www.reddit.com"):
-            url = url[0:8] + 'old' + url[11:]
-        archive_url = archiveis.capture(url)
-        comment_text = f"[Here's]({archive_url}) an archived version of this thread.  \n" \
-                       "[^^Source](https://github.com/kitegi/discount-gv)"
-        submission.reply(comment_text)
-        print("Reply sent")
-    else:
-        sleep(50)
-    sleep(10)
+    try:
+        submission = next(submission_stream)
+        if submission and not submission.is_self:
+            url = submission.url
+            if url.startswith("https://www.reddit.com"):
+                url = url[0:8] + 'old' + url[11:]
+            archive_url = archiveis.capture(url)
+            comment_text = f"[Here's]({archive_url}) an archived version of this thread.  \n" \
+                           "[^^Source](https://github.com/kitegi/discount-gv)"
+            submission.reply(comment_text)
+            print("Reply sent")
+        else:
+            sleep(60)
+    except prawcore.exceptions.ServerError as e:
+        print(e)
+        sleep(300)
